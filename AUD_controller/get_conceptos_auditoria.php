59a2eedf37dbb7b54e8910bdeceb0b8578a9fef1
Desarrollo DRG <138056114+DesarrolloDrg123@users.noreply.github.com>
2026-01-22 14:46:04 +0000

    actualizacion en la consulta de conceptos de evaluacion

--- AUD_controller/get_conceptos_auditoria.php
+++ AUD_controller/get_conceptos_auditoria.php
@@ -6,7 +6,7 @@ try {
     // Consulta para traer los conceptos y el estatus
     // Usamos un CASE para que en el front se vea "Activo" o "Inactivo"
     $sql = "SELECT id, tipo, descripcion, c1, c2, c3, activo,
-            (SELECT SUM(c1) FROM cat_items_auditoria c2 WHERE c2.tipo = cat_items_auditoria.tipo AND c2.activo = 'S') as suma_tipo
+            (SELECT SUM(c1) FROM cat_items_auditoria_aud c2 WHERE c2.tipo = cat_items_auditoria_aud.tipo AND c2.activo = 'S') as suma_tipo
             FROM cat_items_auditoria_aud 
             ORDER BY tipo ASC, id ASC";
 
