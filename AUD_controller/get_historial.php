d8eb174af8bb4d0f1236ebd2ad6567634b705de4
Desarrollo DRG <138056114+DesarrolloDrg123@users.noreply.github.com>
2026-01-22 20:30:15 +0000

    se renombro el archivo y correccion en la consulta

--- AUD_controller/get_historial.php
+++ AUD_controller/get_historial.php
@@ -6,7 +6,7 @@ $sql = "SELECT a.id, a.folio, a.fecha_auditoria, a.calif_total, a.fecha_subida_e
                v.no_serie, v.marca, v.modelo,
                u.nombre as auditor_nombre
         FROM auditorias_vehiculos_aud a
-        JOIN vehiculos v ON a.vehiculo_id = v.id
+        JOIN vehiculos_aud v ON a.vehiculo_id = v.id
         JOIN usuarios u ON a.usuario_id = u.id
         ORDER BY a.fecha_auditoria DESC";
 
